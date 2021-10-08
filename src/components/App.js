682489e7d06e65ece76118360c4483704d281da8
Nick Nemtcev <nicknemtcev@outlook.com>
2021-10-08 04:29:37 -0400

    Add privacy policy page

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,28 @@
 import "./global-styles.css";
 
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Box } from "@chakra-ui/react";
 
 import GameInterface from "./GameInterface";
 import NavBar from "./NavBar/NavBar";
-import { Box } from "@chakra-ui/react";
+import PrivacyPolicy from "./routes/PrivacyPolicy";
+import Leaderboard from "./routes/Leaderboard";
 
 function App() {
   return (
-    <Box id="load-spinner" m={2}>
+    <>
       <NavBar />
-      <GameInterface />
-    </Box>
+      <Router>
+        <Box id="load-spinner" m={2}>
+          <Switch>
+            <Route exact path="/" component={GameInterface} />
+            <Route path="/privacy-policy" component={PrivacyPolicy} />
+            <Route path="/leaderboard" component={Leaderboard} />
+          </Switch>
+        </Box>
+      </Router>
+    </>
   );
 }
 
